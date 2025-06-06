27ef709fd986afdae3e1cb95ffee4dafd147a7db
AbrilArmenta <abrilarpe@gmail.com>
2025-06-06 15:58:52 -0600

    feat: primer avance del formulario.html y formulario.js tomando en cuenta los atributos de objetos.json

--- src/pages/formulario/formulario.js
+++ src/pages/formulario/formulario.js
@@ -3,4 +3,27 @@ import { insertFooter } from "../../../modules/footer/footer.js";
 const homePath ="../../../"
 
 insertHeader(document.getElementById("header"), homePath);
-insertFooter(document.getElementById("footer"));
+insertFooter(document.getElementById("footer")); 
+
+
+// ================================================================================
+const form = document.getElementById('productForm');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault(); // Evita recargar la página
+
+  const nuevoProducto = {
+    nombre: document.getElementById('nombre').value,
+    descripcion: document.getElementById('descripcion').value,
+    cantidad: parseInt(document.getElementById('cantidad').value),
+    precio: parseFloat(document.getElementById('precio').value),
+    imagen: document.getElementById('imagen').value || 'https://via.placeholder.com/150'
+  };
+
+  let productos = JSON.parse(localStorage.getItem('productos')) || [];
+  productos.push(nuevoProducto);
+  localStorage.setItem('productos', JSON.stringify(productos));
+
+  alert('✅ Producto guardado');
+  form.reset(); // Limpia el formulario
+});
