1bc8f3ea1d7759b65f8158f014636cedaa63efe4
AbrilArmenta <abrilarpe@gmail.com>
2025-06-09 17:24:48 -0600

    feat: se extraen los datos del formulario y se hace el objeto json

---
+++ src/pages/registerUser/registerUser.js
@@ -0,0 +1,27 @@
+// Asumiendo que las validaciones ya se hicieron y el formulario es válido
+
+const formulario = document.getElementById("registerUser");
+
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevenir que se recargue la página
+
+ // Crear el objeto directamente mientras se obtienen los valores de los inputs
+  const usuario = {
+    nombreCompleto: document.getElementById("nameUser").value,
+    telefono: document.getElementById("phoneUser").value,
+    email: document.getElementById("emailUser").value,
+    password: document.getElementById("passwordUser").value
+  };
+
+  // Si se quiere en formato JSON para enviar a un servidor:
+  const usuarioJSON = JSON.stringify(usuario);
+  
+ // Guardar en localStorage
+  localStorage.setItem("usuario", usuarioJSON);
+
+ // Confirmar que se guardó correctamente
+   alert("¡Datos guardados en localStorage!");
+
+// Opcional: limpiar el formulario
+  formulario.reset();
+});
