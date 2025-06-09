56491840845c5d35f8ede09f4d0898e822cfb241
Nancy Sánchez <nancyclsanchez@gmail.com>
2025-06-09 16:33:06 -0600

    Feat: Footer.css

---
+++ src/pages/404/error-404.js
@@ -0,0 +1,6 @@
+import { insertHeader } from "../../../modules/header/header.js";
+import { insertFooter } from "../../../modules/footer/footer.js";
+const homePath ="../../../"
+
+insertHeader(document.getElementById("header"), homePath);
+insertFooter(document.getElementById("footer"));
