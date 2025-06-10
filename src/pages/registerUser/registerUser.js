cfe09dcd7e505fa7435bd9a1977294c24aab67ab
AbrilArmenta <abrilarpe@gmail.com>
2025-06-09 18:55:48 -0600

    feat: se añade el header y el footer en el js

--- src/pages/registerUser/registerUser.js
+++ src/pages/registerUser/registerUser.js
@@ -1,3 +1,11 @@
+import { insertHeader } from "../../../modules/header/header.js";
+import { insertFooter } from "../../../modules/footer/footer.js";
+
+const homePath = "../../../";
+insertHeader(document.getElementById("header"), homePath);
+insertFooter(document.getElementById("footer"));
+
+
 // Asumiendo que las validaciones ya se hicieron y el formulario es válido
 
 const formulario = document.getElementById("registerUser");
