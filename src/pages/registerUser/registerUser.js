344d9734781c5d959d6acdbd582cf92018346e54
Luis David <luisdapm007@egmail.com>
2025-06-11 20:18:13 -0600

    fix: ID´s en registerUser.html

--- src/pages/registerUser/registerUser.js
+++ src/pages/registerUser/registerUser.js
@@ -8,31 +8,76 @@ insertFooter(document.getElementById("footer"));
 
 // Asumiendo que las validaciones ya se hicieron y el formulario es válido
 
-const formulario = document.getElementById("registerUser");
+const form = document.getElementById("registerUser");
 
-formulario.addEventListener("submit", function(event) {
+form.addEventListener("submit", event => {
   event.preventDefault(); // Prevenir que se recargue la página
 
  // Crear el objeto directamente mientras se obtienen los valores de los inputs
-  const usuario = {
-    nombreCompleto: document.getElementById("nameUser").value,
-    telefono: document.getElementById("phoneUser").value,
+  const userData = {
+    nameUser: document.getElementById("nameUser").value,
+    phone: document.getElementById("phoneUser").value,
     email: document.getElementById("emailUser").value,
-    password: document.getElementById("passwordUser").value
+    password: document.getElementById("passwordUser").value,
+    passwordConfirm: document.getElementById("passwordConfirm").value,
+    termsAndConditions: document.getElementById("checkConditions").checked,
+    privacityAgreement: document.getElementById("checkPrivacity").checked
   };
 
-  // Si se quiere en formato JSON para enviar a un servidor:
-  const usuarioJSON = JSON.stringify(usuario);
+ 
+  const { nameUser, phone, email, password, passwordConfirm, termsAndConditions, privacityAgreement} = userData;
   
- // Guardar en localStorage
-  localStorage.setItem("usuario", usuarioJSON);
+  //  ================ Validaciones de entradas de usuario ======================================
 
- // Confirmar que se guardó correctamente
-  // alert("¡Datos guardados en localStorage!");
+  let validationName = RegExp("^[A-ZÁÉÍÓÚÑa-záéíóúñ]+(?: [A-ZÁÉÍÓÚÑa-záéíóúñ]+)*$");
+  let validationEmail = RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+  let validationPhone = RegExp("^\\d{10}$");
+  let validationPassword = RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$");
 
- // Mostrar la alerta de Bootstrap
-  document.getElementById("alertaExito").classList.remove("d-none");
+  const alertInput = (message, color, borderColor, backgroundColor) => {
+    alertContainer.innerHTML = `
+        <div class="alert alert-success alert-dismissible fade show" role="alert" style="text-align: center; background-color:${backgroundColor};
+        color:${color}; border-color: ${borderColor};">
+        <strong>${message}</strong>
+        </div>`
 
-// Opcional: limpiar el formulario
-  formulario.reset();
+        setTimeout(() => {
+          alertContainer.innerHTML = ``
+        }, 5000);
+  }
+  
+  if (!validationName.test(nameUser)) {
+    
+    alertInput("¡Verifica que el nombre no contenga números, caracteres especiales o espacios al inicio y al final!", "#FF6F61", "#FF6F61", "#D6E6F2");
+
+  } else if (!validationPhone.test(phone)) {
+
+    alertInput("¡Verifica que el número telefónico contenga 10 dígitos", "#FF6F61", "#FF6F61", "#D6E6F2");
+
+  } else if (!validationEmail.test(email)) {
+
+    alertInput('¡Verifica que tu dirección de correo contenga "@" y/o un dominio válido!', "#FF6F61", "#FF6F61", "#D6E6F2");
+
+  } else if (!validationPassword.test(password)) {
+
+    alertInput("¡Verifica que tu contraseña contenga 8 caracteres como mínimo y al menos una letra mayúscula, un carácter especial y un número!", "#FF6F61", "#FF6F61", "#D6E6F2");
+
+  } else if (passwordConfirm != password ) {
+
+    alertInput("¡Verifica que ambas contraseñas coincidan!", "#FF6F61", "#FF6F61", "#D6E6F2");
+
+  } else if (termsAndConditions != true) {
+
+    alertInput("¡Debes aceptar términos y condiciones para continuar!", "#FF6F61", "#FF6F61", "#D6E6F2");
+
+  } else if (privacityAgreement != true ) {
+
+    alertInput("¡Debes aceptar el acuerdo de privacidad para continuar!", "#FF6F61", "#FF6F61", "#D6E6F2");
+  } else {
+    alertInput("¡Datos enviados exitosamente!", "#0a3622", "#0a3622", "#d1e7dd");
+    const userDataJSON = JSON.stringify(userData);
+    localStorage.setItem("usuario", userDataJSON);
+    form.reset();
+  }
+  
 });
