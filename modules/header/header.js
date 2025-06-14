59827b3ac31931034a81f7208807b54fa3cfc22c
Gustavo Ortega <elitgx@gmail.com>
2025-06-13 21:35:43 -0600

    feat: agregar boton de inicio de sesion y funcionalidad en js

--- modules/header/header.js
+++ modules/header/header.js
@@ -1,8 +1,8 @@
 
 
-const insertHeader = (header, homePath ="./") => {
+const insertHeader = (header, homePath = "./") => {
 
-    header.innerHTML = `
+  header.innerHTML = `
     <!-- Navbar (HEADER) -->
     <nav id="header" class="navbar navbar-expand-lg navbar-light">
       <div class="container-fluid">
@@ -56,10 +56,28 @@ const insertHeader = (header, homePath ="./") => {
               <a class="nav-link" href="${homePath}src/pages/aboutUs/aboutUs.html">Acerca de</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="${homePath}src/pages/ShoppingCart/ShoppingCart.html">Carrito</a>
+                  <a class="nav-link" href="${homePath}src/pages/ShoppingCart/ShoppingCart.html"><svg xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 20 20">
+                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z" />
+                <path
+                  d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+              </svg>
+            </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-mamx" href="${homePath}src/pages/contact/contact.html">Contáctanos</a>
+              <div class="dropdown">
+                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  Cuenta
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item active" href="${homePath}src/pages/inicioSesion/inicioSesion.html">Inicio de sesión</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item" href="${homePath}src/pages/registerUser/registerUser.html">Registro</a></li>
+                </ul>
+              </div>
             </li>
           </ul>
         </div>
@@ -68,4 +86,4 @@ const insertHeader = (header, homePath ="./") => {
     `
 }
 
-export {insertHeader};
+export { insertHeader };
