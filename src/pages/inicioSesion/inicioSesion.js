3afb9565a80fd249df1bd7e562e12bb1f172ef8f
jesus servin <jesusservinmonroy@gmail.com>
2025-06-17 22:14:41 -0600

    fix:inicio de sesion

--- src/pages/inicioSesion/inicioSesion.js
+++ src/pages/inicioSesion/inicioSesion.js
@@ -4,45 +4,37 @@ form.addEventListener("submit", event => {
 
 
     // Crear el objeto directamente mientras se obtienen los valores de los inputs
-    
 
     const Email = document.getElementById("Email").value.trim();
     const password = document.getElementById("password").value.trim();
-   
 
-    
     // Pruebas Validacion de  datos del usuario
     const validationEmail = RegExp (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
     const validationPassword = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/);
 
-
-    console.log("Validaciones ");
     // Validacion de los datos del usuario
     if (!validationEmail.test(Email)) {
         alert("El correo electrónico no es válido.");
-        console.log("Validacion correo");
         return;
     } 
     
     if (!validationPassword.test(password)) {
         alert("La contraseña es incorrecta.");
-        console.log("Validacion Password");
         return;
     }
-      console.log("Post validacion");
 
     const storeUser = JSON.parse(localStorage.getItem("usuario"));
     
     if (!storeUser){
-        alert("No hay registros");
         return;
     }
 
     if (Email == storeUser.email && password == storeUser.password) {//Comprobación de inicio de sesión
-        alert("¡Bienvenudo/a! ¡Has iniciado sesión correctamente!");
-        console.log(" validacion cuenta");
+        alert("¡Bienvenido/a! ¡Has iniciado sesión correctamente!");
+        window.location.href = "../../../index.html"; // Redirige al usuario a la página principal
+        const boolean = true;
+        localStorage.setItem("isLoggedIn", JSON.stringify(boolean)); // Guarda el estado de sesion iniciada
     } else {
         alert("Correo incorrecto");
     }
-        console.log("Post todo");
 });
