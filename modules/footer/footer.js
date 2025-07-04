f8e49db210512a198fb14a8a101afd924e8eff28
jesus servin <jesusservinmonroy@gmail.com>
2025-07-03 20:54:32 -0600

    fix: font de footer

--- modules/footer/footer.js
+++ modules/footer/footer.js
@@ -7,7 +7,7 @@ const insertFooter = (footer, homePath = "./") => {
                 <div class="col-sm-12 col-md-12 col-lg-4 exclusivo">
                     <h3>Exclusivo</h3>
                     <!-- Debe ir a la página de inicio de sesión -->
-                    <a href=""><p>Suscribete y obtén un 10% de descuento</p></a>
+                    <a href="${homePath}src/pages/registerUser/registerUser.html"><p id="exc">Suscribete y obtén un 10% de descuento</p></a>
                 </div>
                 <div class="col-sm-12 col-md-12 col-lg-4 soporte">
                     <h3>Soporte</h3>
@@ -19,7 +19,7 @@ const insertFooter = (footer, homePath = "./") => {
                     <!--<a href="${homePath}src/pages/registerUser/registerUser.html">Mi cuenta</a>
                 </div>-->
                 <div class="col-sm-12 col-md-12 col-lg-4 contacto">
-                    <h3><a href="${homePath}src/pages/contact/contact.html">Contáctanos</a></h3>
+                    <h3><a id="exc" href="${homePath}src/pages/contact/contact.html">Contáctanos</a></h3>
                      <i class="bi bi-envelope">medicinamexico417@gmail.com</i>
                 </div>
             </div>
